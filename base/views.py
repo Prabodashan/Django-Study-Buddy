4913cdf1b94140af621d1addde3b20c50d4a8b89
Prabodashan <prabodashan14@gmail.com>
2023-04-02 19:27:38 +0530

    added update and delete room function

--- base/views.py
+++ base/views.py
@@ -22,7 +22,7 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
-    
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -31,3 +31,31 @@ def createRoom(request):
 
     context ={'form' : form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+
+        form = RoomForm(request.POST ,instance=room)
+    #     topic_name = request.POST.get('topic')
+    #     topic, created = Topic.objects.get_or_create(name=topic_name)
+    #     room.name = request.POST.get('name')
+    #     room.topic = topic
+    #     room.description = request.POST.get('description')
+    if form.is_valid():
+        room.save()
+        return redirect('home')
+
+    context = {'form': form }
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj': room})
